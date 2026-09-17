3e8ab177dc02d32be14d0b78211ee485929955c3
bryansikame2025 <bryansikame2023@gmail.com>
2026-09-17 05:26:01 +0100

    Add files via upload

--- android-template/MainActivity.java
+++ android-template/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sabi.app;
 
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import java.util.Collections;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -14,5 +18,24 @@ public class MainActivity extends BridgeActivity {
       WindowManager.LayoutParams.FLAG_SECURE,
       WindowManager.LayoutParams.FLAG_SECURE
     );
+
+    // Sur Android 10+ avec la navigation par gestes, le systeme intercepte tout glissement
+    // demarre pres du bord de l'ecran pour son propre geste "retour" global, avant meme que la
+    // page web ne recoive l'evenement tactile. On exclut donc une bande verticale sur le bord
+    // gauche de la zone geree par le systeme, pour que le balayage retour de l'app (implemente
+    // en JS, voir index.html) recoive bien le geste a la place.
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      final View racine = getWindow().getDecorView();
+      racine.post(new Runnable() {
+        @Override
+        public void run() {
+          int hauteur = racine.getHeight();
+          int largeurExclusion = (int) (32 * getResources().getDisplayMetrics().density); // ~32dp
+          Rect zone = new Rect(0, 0, largeurExclusion, hauteur);
+          racine.setSystemGestureExclusionRects(Collections.singletonList(zone));
+        }
+      });
+    }
   }
 }
+
